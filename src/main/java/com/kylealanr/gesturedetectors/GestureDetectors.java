39fabee3c85eff0f25ee7c00eb23e79cd70683cd
Kyle Riedemann <kylealanr@gmail.com>
2017-05-08 16:32:39 -0500

    Created abstract class to handle the empty implementations

--- src/main/java/com/kylealanr/gesturedetectors/GestureDetectors.java
+++ src/main/java/com/kylealanr/gesturedetectors/GestureDetectors.java
@@ -1,10 +1,10 @@
 package com.kylealanr.gesturedetectors;
 
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 
+@SuppressWarnings("unused")
 public class GestureDetectors {
-    public static class OnDown implements GestureDetector.OnGestureListener {
+    public static class OnDown extends GestureListener {
         private final $Predicate<MotionEvent> predicate;
 
         public OnDown($Predicate<MotionEvent> predicate) {
@@ -15,34 +15,9 @@ public class GestureDetectors {
         public boolean onDown(MotionEvent e) {
             return predicate != null && predicate.test(e);
         }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            return false;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return false;
-        }
     }
 
-    public static class OnShowPress implements GestureDetector.OnGestureListener {
+    public static class OnShowPress extends GestureListener {
         private final $Function<MotionEvent, Void> function;
 
         public OnShowPress($Function<MotionEvent, Void> function) {
@@ -50,37 +25,12 @@ public class GestureDetectors {
         }
 
         @Override
-        public boolean onDown(MotionEvent e) {
-            return false;
-        }
-
-        @Override
         public void onShowPress(MotionEvent e) {
             function.apply(e);
         }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            return false;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return false;
-        }
     }
 
-    public static class OnSingleTapUp implements GestureDetector.OnGestureListener {
+    public static class OnSingleTapUp extends GestureListener {
         private final $Predicate<MotionEvent> predicate;
 
         public OnSingleTapUp($Predicate<MotionEvent> predicate) {
@@ -88,38 +38,12 @@ public class GestureDetectors {
         }
 
         @Override
-        public boolean onDown(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-
-        }
-
-        @Override
         public boolean onSingleTapUp(MotionEvent e) {
             return predicate != null && predicate.test(e);
-
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            return false;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return false;
         }
     }
 
-    public static class OnScroll implements GestureDetector.OnGestureListener {
+    public static class OnScroll extends GestureListener {
         private final  $QuadPredicate<MotionEvent, MotionEvent, Float, Float> quadPredicate;
 
         public OnScroll($QuadPredicate<MotionEvent, MotionEvent, Float, Float> quadPredicate) {
@@ -127,37 +51,12 @@ public class GestureDetectors {
         }
 
         @Override
-        public boolean onDown(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            return false;
-        }
-
-        @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
             return quadPredicate != null && quadPredicate.test(e1, e2, distanceX, distanceY);
         }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return false;
-        }
     }
 
-    public static class OnLongPress implements GestureDetector.OnGestureListener {
+    public static class OnLongPress extends GestureListener {
         private final $Function<MotionEvent, Void> function;
 
         public OnLongPress($Function<MotionEvent, Void> function) {
@@ -165,66 +64,16 @@ public class GestureDetectors {
         }
 
         @Override
-        public boolean onDown(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            return false;
-        }
-
-        @Override
         public void onLongPress(MotionEvent e) {
             function.apply(e);
         }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return false;
-        }
     }
 
-    public static class OnFling implements GestureDetector.OnGestureListener {
+    public static class OnFling extends GestureListener {
         private final $QuadPredicate<MotionEvent, MotionEvent, Float, Float> quadPredicate;
 
         public OnFling($QuadPredicate<MotionEvent, MotionEvent, Float, Float> quadPredicate) {
             this.quadPredicate = quadPredicate;
-        }
-
-        @Override
-        public boolean onDown(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            return false;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            return false;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-
         }
 
         @Override
---
+++ src/main/java/com/kylealanr/gesturedetectors/GestureListener.java
@@ -0,0 +1,39 @@
+package com.kylealanr.gesturedetectors;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+/**
+ * Default implimentation that does nothing
+ */
+abstract class GestureListener implements GestureDetector.OnGestureListener {
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+}
